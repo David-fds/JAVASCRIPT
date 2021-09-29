c85890ce6ed53a685272e1062a24b8f1b0fdeafc
David Slusher <fdslusher@gmail.com>
2021-09-29 16:36:00 -0600

    first java files

---
+++ HelloWorld.js
@@ -0,0 +1,11 @@
+var firstNumber = 5;
+var secondNumber = 3;
+
+var resultNumber = firstNumber + secondNumber;
+
+console.log(resultNumber);
+
+var eightInText = '8';
+var fiveInText = '5';
+
+console.log(eightInText + fiveInText);
---
+++ jsintro.js
@@ -0,0 +1,29 @@
+var firstInt = 7;
+var secondInt = 8;
+var totalInt = firstInt + secondInt;
+console.log(totalInt);
+
+var firstNumb = 35;
+var secondNumb = 70;
+var totalNumb = secondNumb - firstNumb;
+console.log(totalNumb);
+
+var firstDiv = 100;
+var secondDiv = 25;
+var totalDiv = firstDiv / secondDiv;
+console.log(totalDiv);
+
+var firstFactor = 20;
+var secondFactor = 30;
+var totalFactor = firstFactor * secondFactor;
+console.log(totalFactor);
+
+var firstString = "Hello ";
+var secondString = "World";
+var combined = firstString + secondString;
+console.log(combined);
+
+var thirdString = "My favority food is ";
+var fourthString = "schnitzel auf Wien.";
+var lastString = thirdString + fourthString;
+console.log(lastString);
